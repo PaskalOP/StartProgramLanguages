0a3ff54e4698e89836960c715d92379695d14838
PaskalOP <olgapetrovnapaskal@gmail.com>
2023-03-23 10:49:20 +0200

    Добавили 3 папки

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2)
+{
+    Console.WriteLine($"Ураа число {number1} является квадратом числа {number2}");
+}
+else
+{
+    Console.WriteLine($"Па па па пааамм {number1} не является квадратом числа {number2}");
+}
---
+++ Task03/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите число, которому соответсвует день недели: ");
+string day = Console.ReadLine();
+if (day == "1")
+{
+    Console.WriteLine(" Опять ПОНЕДЕЛЬНИК ((( ");
+}
+else if (day == "2")
+{
+    Console.WriteLine(" Уже ВТОРНИК ( ");
+}
+else if (day == "3")
+{
+    Console.WriteLine(" Среда - маленькая пятница ");
+}
+else if (day == "4")
+{
+    Console.WriteLine(" Сегодня четверг - скоро пятница");
+}
+else if (day == "5")
+{
+    Console.WriteLine(" УРА!!! ПЯТНИЦА!");
+}
+else if (day == "6")
+{
+    Console.WriteLine(" Суббота ");
+}
+else if (day == "7")
+{
+    Console.WriteLine(" Воскресенье - готовься! Завтра ПОНЕДЕЛЬНИК");
+}
+else 
+{
+    Console.WriteLine(" Это вообще не дель недели ");
+}
+
