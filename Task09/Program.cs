1849578d554ccea4814af89de62732028d0e9172
PaskalOP <olgapetrovnapaskal@gmail.com>
2023-03-27 11:59:50 +0300

    Задания с семинара 2

---
+++ Task09/Program.cs
@@ -0,0 +1,30 @@
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int numberRandom = new Random().Next(10,100);
+Console.WriteLine ($"Случайное число из диапазона 10-99: {numberRandom} ");
+// ПЕРВЫЙ ВАРИАНТ
+// int firstDigit = numberRandom/10;
+// int secondDigit = numberRandom%10;
+// if (firstDigit>secondDigit) Console.WriteLine ($"Большая цифра числа {firstDigit}" );
+// else Console.WriteLine ($"Большая цифра числа {secondDigit}" );
+
+//КОРОТКАЯ ЗАПИСЬ
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа => {maxDigit}");
+
+
+// СОЗДАЕМ МЕТОД
+int MaxDigit (int numRandom)
+{
+    int firstDigit = numRandom/10;
+    int secondDigit = numRandom%10;
+    int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return maxDigit;
+}
+int maxDigit = MaxDigit (numberRandom);
+Console.WriteLine($"Наибольшая цифра числа => {maxDigit}");
---
+++ Task11/Program.cs
@@ -0,0 +1,22 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int numRandom = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из диапазона 100-999: {numRandom} ");
+
+int newNum = DelSecondDigit(numRandom );
+Console.WriteLine($"Новое двузначное число: {newNum } ");
+
+
+int DelSecondDigit(int randomNumber)
+{
+    int firstNumber = randomNumber / 100;
+    int thirdNumber = randomNumber % 10;
+    int newNumber = firstNumber * 10 + thirdNumber;
+    return newNumber;
+}
+
